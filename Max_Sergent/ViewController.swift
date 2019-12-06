5d660ae63111d899f5ff9c277b6aba7c9377aa90
maxrgnt <maxrgnt@umich.edu>
2019-12-06 15:04:01 -0500

    v 0.0.1

---
+++ Max_Sergent/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Max_Sergent
+//
+//  Created by Max Sergent on 12/6/19.
+//  Copyright © 2019 Max Sergent. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        print("Hello World!")
+        
+    }
+    
+    
+}
