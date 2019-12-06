22ee218b6a901c24259e7e43f36587b6a19a32b9
maxrgnt <maxrgnt@umich.edu>
2019-12-06 14:54:24 -0500

    Initial Commit

---
+++ Max_SergentTests/Max_SergentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Max_SergentTests.swift
+//  Max_SergentTests
+//
+//  Created by Max Sergent on 12/6/19.
+//  Copyright © 2019 Max Sergent. All rights reserved.
+//
+
+import XCTest
+@testable import Max_Sergent
+
+class Max_SergentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
