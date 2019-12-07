50bcd133cdcada58aa7d743c23e2f2a8e13f36ea
maxrgnt <maxrgnt@umich.edu>
2019-12-07 14:05:05 -0500

    v 0.0.1

---
+++ Max_Sergent/Testing.swift
@@ -0,0 +1,61 @@
+//
+//  Testing.swift
+//  Max_Sergent
+//
+//  Created by Max Sergent on 12/7/19.
+//  Copyright © 2019 Max Sergent. All rights reserved.
+//
+
+import UIKit
+
+class Testing: UIView {
+    
+    //MARK: Definitions
+    // Objects
+    let addToFirebase = UIButton()
+    
+    //MARK: Initialization
+    init() {
+        // Initialize frame of view
+        super.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Setup
+    func setup() {
+        backgroundColor = .darkGray
+        objectSettings()
+        constraints()
+    }
+    
+    func objectSettings() {
+        addSubview(addToFirebase)
+        addToFirebase.backgroundColor = .lightGray
+        addToFirebase.alpha = 0.7
+        addToFirebase.isUserInteractionEnabled = true
+    }
+    
+    //MARK: Constraints
+    func constraints() {
+        addToFirebase.translatesAutoresizingMaskIntoConstraints                                                     = false
+        addToFirebase.leadingAnchor.constraint(equalTo: leadingAnchor).isActive                                     = true
+        addToFirebase.trailingAnchor.constraint(equalTo: trailingAnchor).isActive                                   = true
+        addToFirebase.topAnchor.constraint(equalTo: topAnchor).isActive                                             = true
+        addToFirebase.heightAnchor.constraint(equalToConstant: 300).isActive                                        = true
+    }
+    
+    //MARK: Animations
+    func animate (to state: String) {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut
+            , animations: ({
+                self.layoutIfNeeded()
+            }), completion: { (completed) in
+                // pass
+            }
+        )
+    }
+    
+}
--- Max_Sergent/ViewController.swift
+++ Max_Sergent/ViewController.swift
@@ -12,15 +12,52 @@ import Firebase
 
 class ViewController: UIViewController {
     
+    let testing = Testing()
+    let ref = Database.database().reference()
+    
     override func viewDidLoad() {
-        
         print("Hello World!")
-        
-        let ref = Database.database().reference()
-        print(ref)
-        print(ref.key)
-        
+        setup()
     }
     
+    //MARK: Setup
+    func setup() {
+        objectSettings()
+        constraints()
+    }
+    
+    //MARK: Settings
+    func objectSettings() {
+        view.addSubview(testing)
+        testing.setup()
+        testing.addToFirebase.addTarget(self, action: #selector(sendData), for: .touchUpInside)
+    }
+    
+    //MARK: Constraints
+    func constraints() {
+        testing.translatesAutoresizingMaskIntoConstraints                                                           = false
+        testing.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive                                 = true
+        testing.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive                               = true
+        testing.topAnchor.constraint(equalTo: self.view.topAnchor).isActive                                         = true
+        testing.heightAnchor.constraint(equalToConstant: 600).isActive                                              = true
+    }
+    
+    //MARK: Functionality
+    @objc func sendData() {
+        let dataType = "user"
+        let name = "Max Sergent"
+        let desiredCompany = "Capital One"
+        let dictionary = toAnyObject(name: name, desiredCompany: desiredCompany)
+        let refItem = ref.child(dataType.lowercased())
+        refItem.setValue(dictionary)
+    }
+    
+    func toAnyObject(name: String, desiredCompany: String, completed: Bool? = false) -> Any {
+      return [
+        "name": name,
+        "desiredCompany": desiredCompany,
+        "completed": completed!
+      ]
+    }
     
 }
